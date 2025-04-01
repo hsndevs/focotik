dfc9db19aa694f375252631fae1e84fcfa7294ab
Hasan Tareq <hsntareq@gmail.com>
2025-04-01 21:55:59 +0600

    Video is now passed on changing tabs 1

--- build/blocks/testimonial-item/index.asset.php
+++ build/blocks/testimonial-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5854def10ff9d8a3d61f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e566bb2a25b250e369c6');
